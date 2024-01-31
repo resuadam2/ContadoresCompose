1da6966396cb03c57807c63f5a6dba5191baf4fc
resuadam2 <resuadam2@gmail.com>
2024-01-31 13:49:35 +0100

    adding readme and small changes

---
+++ README.md
@@ -0,0 +1,9 @@
+# Ejemplo de Contadores con Jetpack Compose
+
+Proyecto de ejemplo para mostrar el uso de los contadores en Jetpack Compose.
+
+Estado de variables, eventos y efectos.
+
+Uso de rememberSaveable para guardar el estado de la variable contador.
+
+Basado en un ejemplo del curso de PFFP de @Avidaldo.
--- app/src/main/java/com/example/contadorescompose/MainActivity.kt
+++ app/src/main/java/com/example/contadorescompose/MainActivity.kt
@@ -1,12 +1,8 @@
 package com.example.contadorescompose
 
-import android.graphics.drawable.Icon
 import android.os.Bundle
-import android.widget.EditText
-import android.widget.ImageButton
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -17,7 +13,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.Button
+import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -25,7 +21,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -33,9 +28,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.contadorescompose.ui.theme.ContadoresComposeTheme
 
@@ -88,7 +81,7 @@ fun Contador(countFinal: Int, incrementoTotal: (Int) -> Unit) {
             Text(text = "$count")
             IconButton(onClick = { count = 0 }) {
                 Icon(
-                    Icons.Default.Delete,
+                    Icons.Outlined.Delete,
                     contentDescription = "Delete button",
                 )
             }
